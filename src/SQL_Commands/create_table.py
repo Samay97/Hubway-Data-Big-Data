9c7444c7351e2b2bfce83466cba3228253b63219
Samuel Knoch <samuel@korntal.eu>
2021-11-08 17:53:04 +0100

    add new table for later

--- src/SQL_Commands/create_table.py
+++ src/SQL_Commands/create_table.py
@@ -13,8 +13,25 @@ CREATE EXTERNAL TABLE IF NOT EXISTS hubway_data(
 	end_station_longitude STRING,
     bikeid INT,
     usertype STRING,
-    death_year DECIMAL(4,0),
-    gender death_year DECIMAL(1,0)
+    birth_year DECIMAL(4,0),
+    gender DECIMAL(1,0)
+) COMMENT 'Hubway Data' PARTITIONED BY (partition_year int, partition_month int, partition_day int) ROW FORMAT DELIMITED FIELDS TERMINATED BY '\\t' STORED AS TEXTFILE LOCATION '/user/hadoop/imdb/title_basics'
+TBLPROPERTIES ('skip.header.line.count'='1');
+'''
+
+hiveSQL_create_table_hubway_data_optimized='''
+CREATE EXTERNAL TABLE IF NOT EXISTS hubway_data(
+	tripduration INT,
+	tripdistance INT,
+	starttime TIMESTAMP,
+	stoptime TIMESTAMP,
+	start_station_id INT,
+	start_station_name STRING,
+	end_station_id INT,
+    end_station_name STRING,
+    bikeid INT,
+    birth_year DECIMAL(4,0),
+    gender DECIMAL(1,0)
 ) COMMENT 'Hubway Data' PARTITIONED BY (partition_year int, partition_month int, partition_day int) ROW FORMAT DELIMITED FIELDS TERMINATED BY '\\t' STORED AS TEXTFILE LOCATION '/user/hadoop/imdb/title_basics'
 TBLPROPERTIES ('skip.header.line.count'='1');
 '''
